1b1694125d7335fc5acaabf63382a3d182653914
trainer <trandafilov_vn@mail.ru>
2018-03-13 19:24:03 +0200

    initial commit

---
+++ src/lesson01/HelloWorld.java
@@ -0,0 +1,19 @@
+package lesson01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        boolean b = true;
+        char c    = 'f';
+        int i     = 2;
+        long l    = 5000000088L;
+        float f   = 9.5F;
+        double d  = 9.5;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
